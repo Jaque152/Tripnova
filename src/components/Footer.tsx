0bb93c8a711517c888b6f5a16593f970d8ec433a
Jaque152 <95445582+Jaque152@users.noreply.github.com>
2026-07-09 15:26:35 -0600

    Update contact information in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -65,8 +65,8 @@ export function Footer() {
             <h4 className="font-bold mb-6 text-white uppercase tracking-widest text-xs"><T>Soporte</T></h4>
             <ul className="space-y-4 text-sm font-medium text-background/60">
               <li className="flex items-center gap-3"><Mail className="w-4 h-4 text-primary" /> info@tripnova.com.mx</li>
-              <li className="flex items-center gap-3"><Phone className="w-4 h-4 text-primary" /> +52 55 5555 5555</li>
-              <li className="flex items-center gap-3"><MapPin className="w-4 h-4 text-primary" /> Ciudad de México, MX</li>
+              <li className="flex items-center gap-3"><Phone className="w-4 h-4 text-primary" />  +52 1 55 1708 4774</li>
+              <li className="flex items-center gap-3"><MapPin className="w-4 h-4 text-primary" />  AVENIDA COYOACAN 1878, PISO 14 DEP 1405 - A COLONIA DEL VALLE BENITO JUÁREZ C.P. 03100 CIUDAD DE MEXICO</li>
             </ul>
           </div>
 
